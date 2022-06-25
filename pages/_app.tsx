ad3c402c32d6d5fde8d9c99705e77375a277bcd8
Mohammad Norouzi <mnorouzidev@gmail.com>
2022-06-25 23:08:46 +0430

    Completed the task

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { wrapper } from 'state/store';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default wrapper.withRedux(MyApp);
